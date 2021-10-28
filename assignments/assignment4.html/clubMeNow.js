c47101650eafe56451b6c2b5ee6e6679ba6e5d53
Tanner <67660047+Punkster81@users.noreply.github.com>
2021-10-28 14:03:41 -0400

    Update clubMeNow.js

--- assignments/assignment4.html/clubMeNow.js
+++ assignments/assignment4.html/clubMeNow.js
@@ -83,13 +83,15 @@ function appendTableRows() {
 function displayClubEntry() {
 	
 	
-	window.location.href = "clubEntry.html",true; 
+	window.location.href = "clubEntry.html"; 
 }
 
 // navigate to "Distance Entry" screen (from one of the club "+" buttons)
 function displayClubDistanceEntryForm(c) {
 	localStorage.setItem("club", c); // save chosen club
-	window.location.href = "clubDistanceEntry.html"; // redirect to entry form
+	
+	
+	//window.location.href = "clubDistanceEntry.html"; // redirect to entry form
 }
 
 // replace the current "clubs" array with the previous one
